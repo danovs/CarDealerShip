d444afe6b9b38c82618bd83bc76c68201e1f918f
danovs <dsludanov@gmail.com>
2024-05-09 20:03:38 +0300

    deleteemployee

--- CarDealerShip/EmployeesPage.xaml.cs
+++ CarDealerShip/EmployeesPage.xaml.cs
@@ -1,34 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CarDealerShip
 {
-    /// <summary>
-    /// Логика взаимодействия для EmployeesPage.xaml
-    /// </summary>
     public partial class EmployeesPage : Page
     {
+        private CarDealershipEntities db;
+
         public EmployeesPage()
         {
             InitializeComponent();
-            DGridEmployees.ItemsSource = CarDealershipEntities.GetGontext().employees.ToList();
+            db = new CarDealershipEntities();
+            LoadEmployeeData();
+        }
+
+        private void LoadEmployeeData()
+        {
+            DGridEmployees.ItemsSource = db.employees.ToList();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            // Навигация на страницу редактирования с пустым сотрудником для добавления
             FrameManger.AdminFrame.Navigate(new EditEmployeePage());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (DGridEmployees.SelectedItem != null)
+            {
+                employee selectedEmployee = DGridEmployees.SelectedItem as employee;
+
+                try
+                {
+                    // Подключаем выбранного сотрудника к текущему контексту данных
+                    db.employees.Attach(selectedEmployee);
+                    db.employees.Remove(selectedEmployee);
+                    db.SaveChanges();
+
+                    // Обновляем роль пользователя
+                    var selectedUser = db.users.FirstOrDefault(u => u.user_id == selectedEmployee.user_id);
+                    if (selectedUser != null)
+                    {
+                        selectedUser.role_id = 3;
+                        db.SaveChanges();
+                    }
+
+                    MessageBox.Show("Сотрудник успешно удален и его роль изменена.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при удалении сотрудника: " + ex.Message);
+                }
+
+                // Обновляем список сотрудников после удаления
+                LoadEmployeeData();
+            }
+            else
+            {
+                MessageBox.Show("Выберите сотрудника для удаления.");
+            }
         }
     }
 }
